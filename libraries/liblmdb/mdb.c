2a6846f13ec95dd0aeaf428143b414b6e5195e3a
smlu <smlu@s5.net>
2017-01-11 09:25:48 +0100

    Remove macro definition _CRT_SECURE_NO_WARNINGS

--- libraries/liblmdb/mdb.c
+++ libraries/liblmdb/mdb.c
@@ -47,12 +47,9 @@
 #endif
 
 #ifdef _WIN32
-
-#define _CRT_SECURE_NO_WARNINGS
 #include <malloc.h>
 #include <windows.h>
 #include <VersionHelpers.h>
-#undef  _CRT_SECURE_NO_WARNINGS
 
 #define strdup _strdup
 
