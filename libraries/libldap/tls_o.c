b199ce99cb137bde4d285e2ed96bb1d220a9480c
Ondřej Kuzník <ondra@mistotebe.net>
2026-09-01 10:39:30 +0100

    ITS#10578 libldap: Make sure error stack is empty before a checked SSL_*() call
    Before OpenSSL 4.0, SSL_get_error() would use the thread's error stack
    and a pending error there would stick around essentially forever.

--- libraries/libldap/tls_o.c
+++ libraries/libldap/tls_o.c
@@ -862,6 +862,7 @@ tlso_session_connect( LDAP *ld, tls_session *sess, const char *name_in )
 			return -1;
 	}
 #endif
+	ERR_clear_error();
 	/* Caller expects 0 = success, OpenSSL returns 1 = success */
 	rc = SSL_connect( s ) - 1;
 	return rc;
@@ -873,6 +874,7 @@ tlso_session_accept( tls_session *sess )
 	tlso_session *s = (tlso_session *)sess;
 
 	/* Caller expects 0 = success, OpenSSL returns 1 = success */
+	ERR_clear_error();
 	return SSL_accept( s ) - 1;
 }
 
@@ -1620,6 +1622,7 @@ tlso_sb_read( Sockbuf_IO_Desc *sbiod, void *buf, ber_len_t len)
 
 	p = (struct tls_data *)sbiod->sbiod_pvt;
 
+	ERR_clear_error();
 	ret = SSL_read( p->session, (char *)buf, len );
 #ifdef HAVE_WINSOCK
 	errno = WSAGetLastError();
@@ -1646,6 +1649,7 @@ tlso_sb_write( Sockbuf_IO_Desc *sbiod, void *buf, ber_len_t len)
 
 	p = (struct tls_data *)sbiod->sbiod_pvt;
 
+	ERR_clear_error();
 	ret = SSL_write( p->session, (char *)buf, len );
 #ifdef HAVE_WINSOCK
 	errno = WSAGetLastError();
